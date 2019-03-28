a7b286b6473f950f838d0f1298bbfc3de7358d1c
Anupam Mahapatra <Anupam.Mahapatra@rci.rogers.com>
2019-03-28 13:54:26 -0400

    clean up

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-#build a new image and run it
-docker build -t myjenkins:latest .
-#Here i am looking into a local folder
-#username: anupam || password: password123
-docker run --name myjenkins -p 6060:8080 -p 30000:50000 -v /Users/anupam.mahapatra/gitstuff/JenkinsHome2:/var/jenkins_home myjenkins:latest
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Kill and remove existing container/images
-docker kill myjenkins
-docker container rm myjenkins
